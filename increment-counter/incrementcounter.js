1ca1642ea27df95c94c8fc24c345a2cba2569d0f
arbaz <arbazkhan971@gmail.com>
2023-07-09 22:10:20 +0530

    added changes

---
+++ increment-counter/incrementcounter.js
@@ -0,0 +1,21 @@
+const counters = document.querySelectorAll('.counter');
+
+console.log(counters)
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+    let target = +counter.getAttribute('data-target');
+    console.log(target)
+    let inc = target / 20;
+    let count = 0;
+    function incrementcounter(){
+        if( count < target){
+            count = count + inc;
+            counter.innerText = count;
+            setInterval(incrementcounter, 1);
+        }else{
+            counter.innerText = target;
+        }
+    }
+    incrementcounter();
+})
